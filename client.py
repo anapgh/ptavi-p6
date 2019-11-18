0f9bd59da4dd95843522cbafa63831637e64134d
anapgh <ap.garcia.2016@alumnos.urjc.es>
2019-11-18 15:47:39 +0100

    simplificado if del metodo

--- client.py
+++ client.py
@@ -19,17 +19,16 @@ try:
 except (IndexError, ValueError):
     sys.exit('Usage: python3 client.py method receiver@IP:SIPport')
 
+if METHOD == 'INVITE' or 'BYE':
+    SIP = (METHOD + ' sip:' + user + '@' + ip + 'SIP/2.0')
+else:
+    SIP = METHOD
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((ip, int(port)))
 
-    print("Enviando: " + SIP)
-    my_socket.send(bytes(SIP, 'utf-8') + b'\r\n')
+    my_socket.send(bytes(SIP , 'utf-8') + b'\r\n\r\n')
     data = my_socket.recv(1024)
-
-    print('Recibido -- ', data.decode('utf-8'))
-    print("Terminando socket...")
-
-print("Fin.")
+    print(data.decode('utf-8'))
