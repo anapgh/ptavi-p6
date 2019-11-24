c769a3a58f6adb2cfdc2e9241daab4bdc0267ff6
anapgh <ap.garcia.2016@alumnos.urjc.es>
2019-11-24 22:37:47 +0100

    pasado el pep8

--- client.py
+++ client.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente que abre un socket a un servidor
-"""
+"""Programa cliente que abre un socket a un servidor"""
 
 import socket
 import sys
 
-# Cliente UDP simple.
+""" Cliente UDP simple."""
 
 try:
     METHOD = sys.argv[1]
@@ -20,21 +18,25 @@ except (IndexError, ValueError):
     sys.exit('Usage: python3 client.py method receiver@IP:SIPport')
 
 if METHOD == 'INVITE' or 'BYE':
-    SIP = (METHOD + ' sip:' + user + '@' + ip + ' SIP/2.0')
+    LINE = (METHOD + ' sip:' + user + '@' + ip + ' SIP/2.0')
 else:
-    SIP = METHOD
+    LINE = METHOD
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+""" Creamos el socket, lo configuramos y lo atamos a un servidor/puerto"""
+
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((ip, int(port)))
 
-    my_socket.send(bytes(SIP , 'utf-8') + b'\r\n\r\n')
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n\r\n')
     data = my_socket.recv(1024)
     request = data.decode('utf-8')
     print('Recibido -- ', request)
+
+    """ Enviamos el mensaje ACK """
+
     if METHOD == 'INVITE':
         request = request.split('\r\n\r\n')[2]
         if request == 'SIP/2.0 200 OK':
             LINE = ('ACK' + ' sip:' + user + '@' + ip + ' SIP/2.0')
-            my_socket.send(bytes(LINE , 'utf-8') + b'\r\n\r\n')
+            my_socket.send(bytes(LINE, 'utf-8') + b'\r\n\r\n')
